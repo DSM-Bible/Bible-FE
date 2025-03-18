4950eb37485970c4856a3512579e24faadd0a1c0
은지 <wej0513@dsm.hs.kr>
2025-03-18 09:02:13 +0900

    ⚙️ :: setting

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { QueryClientProvider, QueryClient } from "react-query";
+import "./Styles/index.css";
+import Router from "./Router";
+
+function App() {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router />;
+    </QueryClientProvider>
+  );
+}
+
+export default App;
