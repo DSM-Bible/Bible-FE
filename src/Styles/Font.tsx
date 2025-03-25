df159008393a8f46adb5438196c66fe7a1486b69
은지 <wej0513@dsm.hs.kr>
2025-03-25 20:27:54 +0900

    ⚙️ :: fontsetting

---
+++ src/Styles/Font.tsx
@@ -0,0 +1,60 @@
+import { css } from "@emotion/react";
+import { Color } from "./Color";
+
+interface FontPropsType {
+  text?: string;
+  kind?: keyof typeof fonts;
+  color?: keyof typeof Color;
+}
+
+export const Font = ({
+  text,
+  kind = "",
+  color = "defaultBlack",
+}: FontPropsType) => {
+  return <p css={[fonts[kind], { color: Color[color] }]}>{text}</p>;
+};
+
+const fonts: { [key: string]: ReturnType<typeof css> } = {
+  "": css``,
+  header: css`
+    font-size: 25px;
+    font-weight: 600;
+  `,
+  headLine1: css`
+    font-size: 25px;
+    font-weight: 600;
+  `,
+  headLine2: css`
+    font-size: 20px;
+    font-weight: 600;
+  `,
+  bodyTItle: css`
+    font-size: 18px;
+    font-weight: 600;
+  `,
+  bodyText1: css`
+    font-size: 15px;
+    font-weight: 500;
+  `,
+  bodyText2: css`
+    font-size: 12px;
+    font-weight: 400;
+  `,
+  bodyText3: css`
+    font-size: 10px;
+    font-weight: 400;
+  `,
+  description: css`
+    font-size: 14px;
+    font-weight: 400;
+  `,
+  btnText: css`
+    font-size: 20px;
+    font-weight: 500;
+  `,
+  modalText: css`
+    font-size: 18px;
+    font-weight: 400;
+  `,
+};
