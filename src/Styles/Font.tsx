da456d8782c77b832d44b63fe382b175db359c4a
은지 <wej0513@dsm.hs.kr>
2025-03-26 10:01:43 +0900

    ⚙️ :: font @emotion/styled로 수정

--- src/Styles/Font.tsx
+++ src/Styles/Font.tsx
@@ -1,4 +1,4 @@
-import { css } from "@emotion/react";
+import styled from "@emotion/styled";
 import { Color } from "./Color";
 
 interface FontPropsType {
@@ -12,48 +12,49 @@ export const Font = ({
   kind = "",
   color = "defaultBlack",
 }: FontPropsType) => {
-  return <p css={[fonts[kind], { color: Color[color] }]}>{text}</p>;
+  const StyledFont = fonts[kind] || fonts[""];
+  return <StyledFont color={color}>{text}</StyledFont>;
 };
 
-const fonts: { [key: string]: ReturnType<typeof css> } = {
-  "": css``,
-  header: css`
+const fonts = {
+  "": styled.p``,
+  header: styled.p`
     font-size: 25px;
     font-weight: 600;
   `,
-  headLine1: css`
+  headLine1: styled.p`
     font-size: 25px;
     font-weight: 600;
   `,
-  headLine2: css`
+  headLine2: styled.p`
     font-size: 20px;
     font-weight: 600;
   `,
-  bodyTItle: css`
+  bodyTItle: styled.p`
     font-size: 18px;
     font-weight: 600;
   `,
-  bodyText1: css`
+  bodyText1: styled.p`
     font-size: 15px;
     font-weight: 500;
   `,
-  bodyText2: css`
+  bodyText2: styled.p`
     font-size: 12px;
     font-weight: 400;
   `,
-  bodyText3: css`
+  bodyText3: styled.p`
     font-size: 10px;
     font-weight: 400;
   `,
-  description: css`
+  description: styled.p`
     font-size: 14px;
     font-weight: 400;
   `,
-  btnText: css`
+  btnText: styled.p`
     font-size: 20px;
     font-weight: 500;
   `,
-  modalText: css`
+  modalText: styled.p`
     font-size: 18px;
     font-weight: 400;
   `,
