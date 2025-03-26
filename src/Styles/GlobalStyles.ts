f1dc8dcc154c6c51632c38206f2093c8194ef252
은지 <wej0513@dsm.hs.kr>
2025-03-26 15:47:20 +0900

    ⚙️ :: globalstayles 수정
    jsx 방식으로 적용

--- src/Styles/GlobalStyles.ts
+++ src/Styles/GlobalStyles.ts
@@ -1,9 +1,15 @@
-import { css } from "@emotion/react";
+import { Global, css } from "@emotion/react";
+import { jsx as _jsx } from "react/jsx-runtime";
 
-export const globalStyles = css`
-  * {
-    margin: 0;
-    padding: 0;
-    font-family: Pretendard;
-  }
-`;
+const GlobalStyle = () =>
+  _jsx(Global, {
+    styles: css`
+      * {
+        margin: 0;
+        padding: 0;
+        font-family: Pretendard, sans-serif;
+      }
+    `,
+  });
+
+export default GlobalStyle;
