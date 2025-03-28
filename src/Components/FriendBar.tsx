9e595daf36af499d433d4b656a3718322889939f
hoseon07 <a01081930429@gmail.com>
2025-03-28 10:54:07 +0900

    💄 :: FriendBar 퍼블리싱

---
+++ src/Components/FriendBar.tsx
@@ -0,0 +1,41 @@
+import { css } from "@emotion/react";
+import ProfileImg from "../Assets/Profile.svg";
+import { Font } from "../Styles/Font";
+import { Color } from "../Styles/Color";
+
+export const Profile = () => {
+  return (
+    <div css={Container}>
+      <img src={ProfileImg} alt="" />
+      <div>
+        <Font text="UserName" kind="bodyText1" color="defaultBlack" />
+        <Font text="UserId" kind="bodyText2" color="disableGray" />
+      </div>
+      <div css={Delete}>
+        <Font text="친구 삭제" kind="bodyText3" color="defaultRed" />
+      </div>
+    </div>
+  );
+};
+
+const Container = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 340px;
+  height: 65px;
+  padding-left: 15px;
+  gap: 15px;
+  border-bottom: 1px solid ${Color.disableGray};
+`;
+
+const Delete = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 70px;
+  height: 20px;
+  border: 1px solid ${Color.defaultRed};
+  border-radius: 10px;
+  margin-left: 50px;
+`
