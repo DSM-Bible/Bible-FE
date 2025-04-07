ce30defe8fc4b2a34abe7ec3c5de82fcc3e53c59
은지 <wej0513@dsm.hs.kr>
2025-04-08 08:41:53 +0900

    ✨ :: 친구 관리 퍼블리싱

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,15 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
-import { ContentInput } from "./Components/ContentInput";
+import { ManageFriendPage } from "./Pages/ManageFriendPage";
+import { Layout } from "./Components/Layout";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ContentInput />} />
+        <Route path="" element={<></>} />
+        <Route element={<Layout />}>
+          <Route path="/manageFriend" element={<ManageFriendPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
