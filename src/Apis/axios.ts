a3829ce17557f758fd517e755f0d6a151bd30ab2
은지 <wej0513@dsm.hs.kr>
2025-04-08 15:01:37 +0900

    ⚙️ :: API setting

---
+++ src/Apis/axios.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { Cookie } from "../Utils/cookie";
+
+export const instance = axios.create({
+  baseURL: import.meta.env.VITE_APP_BASE_URL,
+  timeout: 3000,
+});
+
+instance.interceptors.request.use(
+  (config) => {
+    const token = Cookie.get("token");
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  (err) => {
+    alert("오류가 발생했습니다");
+    return Promise.reject(err);
+  }
+);
+
+instance.interceptors.response.use(
+  (res) => {
+    return res;
+  },
+  (err) => {
+    console.log(err);
+  }
+);
