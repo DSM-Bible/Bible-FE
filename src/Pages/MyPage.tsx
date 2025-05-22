84ba2183e7fd474d938622083091d190fe9cad3d
은지 <wej0513@dsm.hs.kr>
2025-05-22 19:23:41 +0900

    ✨ ::  회원정보 수정, 유저 정보 API 연동

--- src/Pages/MyPage.tsx
+++ src/Pages/MyPage.tsx
@@ -3,8 +3,9 @@ import { Font } from "../Styles/Font";
 import { Color } from "../Styles/Color";
 import editCheck from "../Assets/img/SVG/editCheck.svg";
 import profileEdit from "../Assets/img/SVG/profileEdit.svg";
-import { useRef, useState } from "react";
-import bonobono from "../Assets/img/PNG/bonobono.png";
+import { useEffect, useRef, useState } from "react";
+import DefaultProfileImg from "../Assets/img/SVG/DefaultProfileImg.svg";
+import { UserInfo, UserUpdate } from "../Apis/account";
 
 const Social = [
   { title: "친구 추가" },
@@ -19,12 +20,52 @@ const setting = [
 
 export const MyPage = () => {
   const [editMode, setEditMode] = useState(false);
-  const [nickname, setNickname] = useState("8글자딱맞춘닉넴님");
-  const [image, setImage] = useState(bonobono);
+  const [nickname, setNickname] = useState("");
+  const [image, setImage] = useState("");
+  const [userId, setUserId] = useState("");
+  const [imageFile, setImageFile] = useState<File | null>(null);
   const fileInputRef = useRef<HTMLInputElement>(null);
 
+  useEffect(() => {
+    const getUserInfo = async () => {
+      try {
+        const response = await UserInfo();
+        setNickname(response.data.nickname);
+        setImage(
+          response.data.profile === "BASE_PROFILE_URL"
+            ? DefaultProfileImg
+            : response.data.profile
+        );
+        setUserId(response.data.userId);
+        console.log(setImage);
+
+        setEditMode(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getUserInfo();
+  }, []);
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setImage(imageUrl);
+      setImageFile(file);
+    }
+  };
+
   const handleEditClick = () => setEditMode(true);
-  const handleSaveClick = () => setEditMode(false);
+
+  const handleSaveClick = async () => {
+    try {
+      await handleUpdate();
+      setEditMode(false);
+    } catch (error) {
+      console.error("회원정보 수정 실패", error);
+    }
+  };
 
   const handleNicknameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNickname(e.target.value);
@@ -36,12 +77,23 @@ export const MyPage = () => {
     }
   };
 
-  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    if (file) {
-      const imageUrl = URL.createObjectURL(file);
-      setImage(imageUrl);
+  const handleUpdate = async () => {
+    const requestData = {
+      nickname,
+    };
+
+    const formData = new FormData();
+
+    formData.append(
+      "body",
+      new Blob([JSON.stringify(requestData)], { type: "application/json" })
+    );
+
+    if (imageFile) {
+      formData.append("file", imageFile);
     }
+
+    await UserUpdate(formData);
   };
 
   return (
@@ -78,7 +130,7 @@ export const MyPage = () => {
               <img css={Update} src={profileEdit} onClick={handleEditClick} />
             )}
           </div>
-          <Font text="@LoginIdMaxLength" kind="bodyText1" color="disableGray" />
+          <Font text={`@${userId}`} kind="bodyText1" color="disableGray" />
         </div>
       </div>
       <div css={TextWrapper}>
@@ -152,6 +204,7 @@ const NickName = css`
 const Update = css`
   width: 20px;
   height: 20px;
+  cursor: pointer;
 `;
 
 const Input = css`
